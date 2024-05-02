525f7c60e8ef7906f1b83d08bd76df2398af498a
krizcollege <133582687+krizcollege@users.noreply.github.com>
2024-05-02 10:02:33 +0800

    index.js

--- index.js
+++ index.js
@@ -3,18 +3,11 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-    if (display.value === "0" && input === "0") {
-        return; // Don't append if both are "0"
-    }
-    if (display.value === "0" && input !== "0") {
-        display.value = input; // Replace "0" with the input
-    } else {
         display.value += input;
-    }
 }
 
 function clearDisplay(){
-    display.value = "";
+        display.value = "";
 }
 
 function calculate(){
